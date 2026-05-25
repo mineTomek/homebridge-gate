ffe720f20a61aa08ce3c89c218bed93f5f577986
mineTomek <lukom.tomek@gmail.com>
2026-05-25 22:39:59 +0200

    Add a type for plugin's config for easier usage

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import { PlatformConfig } from 'homebridge';
+
+export type Config = PlatformConfig & {
+    mqttHost: string;
+    mqttPort: number;
+    mqttUsername: string;
+    mqttPassword: string;
+}
