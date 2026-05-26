5b45ad99ea0b1f7857b68c0b58b31fb4151cd35c
mineTomek <lukom.tomek@gmail.com>
2026-05-26 12:22:26 +0200

    Change the plugin name to match package.json name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'GateAutomation';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-gate';
+export const PLUGIN_NAME = '@minetomek/homebridge-gate';
